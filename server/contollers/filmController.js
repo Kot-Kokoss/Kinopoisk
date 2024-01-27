d0dfab3b8bda4f524c3148450f3c5e3f0e825ca6
Илья Заостровский <112894177+Kot-Kokoss@users.noreply.github.com>
2024-01-27 16:24:53 +0500

    Update filmController.js

--- server/contollers/filmController.js
+++ server/contollers/filmController.js
@@ -2,10 +2,10 @@ const {Film} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class FilmController {
-    async create(req, res) {
+    async create(req, res, next) {
         try {
             const {id, name, genre_id, description, duration, release_date, format_id, faces_id, nominations_id, awards_id} = req.body
-            const film = await Faces.create({id, name, genre_id, description, duration, release_date, format_id, faces_id, nominations_id, awards_id})
+            const film = await Film.create({id, name, genre_id, description, duration, release_date, format_id, faces_id, nominations_id, awards_id})
             
             return res.json(film)
         }
